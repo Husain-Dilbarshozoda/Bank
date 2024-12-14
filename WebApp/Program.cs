f26286ec823cb69d53aee937cab7774ffe4778e2
KOBILOV-CHORI <sanjarkobilov2007@gmail.com>
2024-12-14 20:19:59 +0500

    first commit

---
+++ WebApp/Program.cs
@@ -0,0 +1,22 @@
+using Domain.Entities;
+using Infratructure.DataContext;
+using Infratructure.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.Services.AddScoped<IContext,DapperContext>();
+builder.Services.AddScoped<IGenericService<Customer>, CustomerService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "BankManagment"));
+}
+
+app.MapControllers();
+app.Run();
+
